b11d97b5034b984817f6516499a3a91455dd6b32
m1rcus <91177069+m1rcus@users.noreply.github.com>
2023-05-16 20:22:13 +0300

    Create 5.3.10.1.cpp

---
+++ 5.3.10.1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class FlightBooking {
+public:
+    FlightBooking(int id, int capacity, int reserved);
+    void printStatus();
+private:
+    int id;
+    int capacity;
+    int reserved;
+};
+
+void FlightBooking::printStatus()
+{
+    double percentage = static_cast<double>(reserved) / capacity * 100;
+    cout << "Flight " << id << " : " << reserved << "/" << capacity << " (" << fixed << setprecision(2) << percentage << "%) seats reserved" << endl;
+}
+
+FlightBooking::FlightBooking(int id, int capacity, int reserved)
+    : id(id), capacity(capacity), reserved(reserved)
+{
+}
+
+int main()
+{
+    int reserved = 0, capacity = 0;
+    cout << "Provide flight capacity: ";
+    cin >> capacity;
+    cout << "Provide number of reserved seats: ";
+    cin >> reserved;
+    FlightBooking booking(1, capacity, reserved);
+    booking.printStatus();
+    return 0;
+}
